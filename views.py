f7c219cd4fa4775d295f00ba5f0739153732b9fb
Dmitriy120494 <103331867+Dmitriy120494@users.noreply.github.com>
2023-02-17 22:20:40 +0300

    Delete views.py

--- views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse('У меня получилось! А я Вам говорил!!')
-
-
-def second_page(request):
-    return HttpResponse('А это вторая страница!')
