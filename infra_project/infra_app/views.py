096f3d98c80d1732bb9ed215b012ff877a143bce
Dmitriy120494 <103331867+Dmitriy120494@users.noreply.github.com>
2023-02-17 22:21:48 +0300

    Add files via upload

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось! А я Вам говорил!!')
 
 
 def second_page(request):
